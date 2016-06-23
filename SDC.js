954000a548499ba5acc72d4ae2a0d108c308193a
takadimi <ethan.woodward@gmail.com>
2016-06-23 11:58:21 -0400

    Added default 'anchor' functions. Removed trailing test function. Added function to set container position.

--- SDC.js
+++ SDC.js
@@ -1,8 +1,14 @@
 var SDC = {
+	/*** Text Area Options ***/
 	hotkey:		     "126",
 	backgroundColor: "#000",
 	textColor:	     "#E2B279",
 	height:			 "300px",
+
+	/*** Container Options ***/
+	anchorH:         "left",   // Either left or right.
+	anchorV:         "bottom", // Either bottom or top.
+	width:           "100%",
 
 	applyOptions: function(options) {
 		for (var key in options) {
@@ -13,17 +19,35 @@ var SDC = {
 	},
 
 	init: function(options) {
-		/*** Apply options ***/
 		SDC.applyOptions(options);
 
 		/*** Create Elements ***/
 		var debugContainerElement               = document.createElement("div");
 		debugContainerElement.id                = "debug_container";
 		debugContainerElement.style.display     = "none";
-		debugContainerElement.style.width       = "100%";
+		debugContainerElement.style.width       = SDC.width;
+		debugContainerElement.style.height      = SDC.height;
 		debugContainerElement.style.position    = "absolute";
-		debugContainerElement.style.bottom      = "0";
-		debugContainerElement.style.left        = "0";
+
+		switch (SDC.anchorH) {
+			case "right":
+				debugContainerElement.style.right = "0";
+				break;
+			case "left":
+			default:
+				debugContainerElement.style.left = "0";
+				break;
+		}
+		switch (SDC.anchorV) {
+			case "top":
+				debugContainerElement.style.top = "0";
+				break;
+			case "bottom":
+			default:
+				debugContainerElement.style.bottom = "0";
+				break;
+		}
+
 		debugContainerElement.style.zIndex      = "9999";
 
 		var debugHeaderBarElement               = document.createElement("div");
@@ -53,9 +77,17 @@ var SDC = {
 			if (e.which == SDC.hotkey) {
 				if (debugContainerElement.style.display === "none") {
 					debugContainerElement.style.display = "block";
+					SDC.scrollToBottom();	
 				} else {
 					debugContainerElement.style.display = "none";
 				}
+			}
+
+			if (debugContainerElement.style.display === "block") {
+				if (e.which == "119") SDC.anchorTop("100%", "200px");
+				if (e.which == "97") SDC.anchorLeft("35%", "98%");
+				if (e.which == "115") SDC.anchorBottom("100%", "200px");
+				if (e.which == "100") SDC.anchorRight("35%", "98%");
 			}
 		});
 	},
@@ -68,6 +100,44 @@ var SDC = {
 		temp += str;
 
 		return temp;
+	},
+
+	setContainerPosition: function(top, right, bottom, left, width, height) {
+		var debugContainerElement = document.getElementById("debug_container");
+		debugContainerElement.style.top    = top;
+		debugContainerElement.style.right  = right;
+		debugContainerElement.style.bottom = bottom;
+		debugContainerElement.style.left   = left;
+		debugContainerElement.style.width  = width;
+		debugContainerElement.style.height = height;
+
+		var debugTextAreaElement = document.getElementById("debug_text_area");
+		debugTextAreaElement.style.height  = height;
+
+		SDC.scrollToBottom();
+	},
+
+	anchorRight: function(width, height) {
+		SDC.setContainerPosition(0, 0, "", "", width, height);
+	},
+
+	anchorLeft: function(width, height) {
+		SDC.setContainerPosition(0, "", "", 0, width, height);
+	},
+
+	anchorBottom: function(width, height) {
+		SDC.setContainerPosition("", "", 0, 0, width, height);
+	},
+
+	anchorTop: function(width, height) {
+		SDC.setContainerPosition(0, "", "", 0, width, height);
+	},
+
+	scrollToBottom: function() {
+		var debugTextAreaElement = document.getElementById("debug_text_area");
+		if (debugTextAreaElement.scrollTop != debugTextAreaElement.scrollHeight) {
+			debugTextAreaElement.scrollTop = debugTextAreaElement.scrollHeight;
+		}
 	},
 
 	log: function(message) {
@@ -87,16 +157,6 @@ var SDC = {
 
 		debugTextAreaElement.textContent = debugTextAreaElement.textContent + prepend + timestamp + message + "\n";
 
-		debugTextAreaElement.scrollTop = debugTextAreaElement.scrollHeight;
+		SDC.scrollToBottom();
 	}
 };
-
-function testLog() {
-	var test_obj = {
-		one: "ONE",
-		two: 2,
-		three: [ 1, 2, 3 ]
-	}
-
-	SDC.log(JSON.stringify(test_obj));
-}
